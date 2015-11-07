29f1f7a6b9c1baf3348b948949b6deb534b976b2
Piotr Solnica <piotr.solnica@gmail.com>
2015-11-07 18:22:23 +0100

    Add a comment to page object

--- core/blog/page.rb
+++ core/blog/page.rb
@@ -1,4 +1,7 @@
 module Blog
+  # This is our main layout context that we inject into view
+  #
+  # Methods available in this object will be available in the main layout
   class Page
     attr_reader :config
 

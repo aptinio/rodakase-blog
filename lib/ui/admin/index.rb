88404c3aaab75ac0ad078951a550646bc4da9658
Piotr Solnica <piotr.solnica@gmail.com>
2015-11-08 14:18:17 +0100

    Import posts repo into index view

--- lib/ui/admin/index.rb
+++ lib/ui/admin/index.rb
@@ -1,20 +1,18 @@
+require 'blog/import'
 require 'blog/view'
 
 module Ui
   module Admin
     class Index < Blog::View::Admin
+      include Blog::Import('persistence.repositories.posts')
+
       configure do |config|
         config.template = 'admin/posts'
       end
 
       def call(page, locals = {})
         # TODO: add support for building locals hash so we don't have to override call
-        super(page, locals.merge(posts: posts))
-      end
-
-      def posts
-        # TODO: add support for Import to View objects
-        Blog::Container['persistence.repositories.posts'].index
+        super(page, locals.merge(posts: posts.index))
       end
     end
   end

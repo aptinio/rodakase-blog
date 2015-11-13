963b01f7fcc69c540a97fb9df0866cc602227789
Piotr Solnica <piotr.solnica@gmail.com>
2015-11-13 10:37:58 +0100

    Simplify rom container registration

--- core/boot/rom.rb
+++ core/boot/rom.rb
@@ -5,19 +5,11 @@ Blog::Container.namespace('persistence') do |container|
   ROM.use(:auto_registration)
   ROM.setup(:sql, container.config.app.database_url)
 
-  container.register('rom') do
-    if ROM.container
-      ROM.container
-    else
-      container['persistence.setup']
-
-      %w(relations commands).each do |type|
-        container.require("lib/persistence/#{type}/**/*.rb")
-      end
-
-      ROM.finalize.container
-    end
+  %w(relations commands).each do |type|
+    container.require("lib/persistence/#{type}/**/*.rb")
   end
+
+  container.register('rom', ROM.finalize.container)
 
   container.auto_register!('lib/persistence/repositories') do |repo|
     -> { repo.new(container['persistence.rom']) }
